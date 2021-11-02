5176972603adf1757ed0d48ccb02d2c05cffbf13
Laura Gallardo <laura_gallardo@bio-rad.com>
2021-11-02 11:49:22 -0600

    renaming

--- src/config/test.js
+++ src/config/test.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Test() {
     const pwd = "user123";
-    const pwd = "user123";
+    const pwd = "pwd";
     return (
         <button>button</button>
     );
