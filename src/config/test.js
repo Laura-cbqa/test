ed03e4111a4b11247fda084a65891c99b0ff3d2e
Laura Gallardo <laura@cbqasolutions.com>
2021-11-04 12:51:31 -0600

    duplication and trash data

--- src/config/test.js
+++ src/config/test.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 export default function Test() {
     const pwd = "user12356";
+    const unusedvar;
+    const unusedvar;
     return (
         <button>button</button>
     );
